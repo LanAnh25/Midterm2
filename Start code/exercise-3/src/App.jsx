808066714c9707e5c9b9e096cbf4eab7f89c9a6a
LanAnh <anh.pham25@student.passerellesnumeriques.org>
2024-06-08 10:51:10 +0700

    bai3

--- Start code/exercise-3/src/App.jsx
+++ Start code/exercise-3/src/App.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  /* You will need to use many state to keep the inut values and other needs */
+ 
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+  const [result, setResult] = useState("");
+  const [error, setError] = useState(false);
 
-  /* You will need some function to handle the key pressed and button events */
+ 
+  const onA = (e) => {
+    setA(e.target.value);
+  };
+
+
+  const onB = (e) => {
+    setB(e.target.value);
+  };
+
+
+  const computeSum = () => {
+    const numA = parseFloat(a);  
+    const numB = parseFloat(b); 
+
+    if (isNaN(numA) || isNaN(numB)) {     
+      setError(true);
+      setResult("A and B shall be numbers !");
+    } else {
+      setError(false);
+      setResult(numA + numB);
+    }
+  };
 
   return (
     <main>
@@ -16,10 +42,9 @@ function App() {
       <input onKeyUp={onB} />
 
       <label>A + B =</label>
+      <input disabled value={result} style={{ color: error ? "red" : "black" }} />
 
-      {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <button onClick={computeSum}>Compute</button>
     </main>
   );
 }
